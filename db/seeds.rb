2213308dfefbc751684931630855511072845063
Jay Vana <jsvana@mtu.edu>
2015-01-27 21:09:30 -0800

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,19 +4,15 @@ Request.create({
 	name: 'Pirates of the Caribbean: The Curse of the Black Pearl',
 	medium: 'movie',
 	votes: 5,
-	active: true
-})
-
-Request.create({
-	name: 'The Grand Budapest Hotel',
-	medium: 'movie',
-	votes: 6,
-	active: true
-})
-
-Request.create({
-	name: 'Modern Vampires of the City',
-	medium: 'movie',
-	votes: 2,
+	rating: 'PG-13',
+	art: 'http://content9.flixster.com/movie/11/15/76/11157635_tmb.jpg',
+	link_service: 'Rotten Tomatoes',
+	year: '2003',
+	runtime: 143,
+	audience_score: 86,
+	critics_score: 79,
+	external_id: '10020',
+	external_service: 'Rotten Tomatoes',
+	link: 'http://api.rottentomatoes.com/api/public/v1.0/movies/10020.json',
 	active: true
 })
