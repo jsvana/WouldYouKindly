d7e2efed41ab93275f58f259c2a34f2f4a22b711
Jay Vana <jsvana@mtu.edu>
2015-01-27 21:01:36 -0800

    Add request counts in headers (ref #3)

--- app/assets/javascripts/requests/index.controller.js
+++ app/assets/javascripts/requests/index.controller.js
@@ -1,5 +1,6 @@
 angular.module('kindly.requests').controller('index',
-  ['$scope', 'Request', function($scope, Request) {
+  ['$scope', '$filter', 'Request', function($scope, $filter, Request) {
+    $scope.requests = [];
     Request.query().then(function (requests) {
       $scope.requests = requests;
     });
@@ -9,5 +10,15 @@ angular.module('kindly.requests').controller('index',
     $scope.upvote = function(request) {
       request.votes += 1;
     };
+
+    $scope.typeCount = function(type) {
+      var count = 0;
+      for (r in $scope.requests) {
+        if ($scope.requests[r].medium === type) {
+          count += 1;
+        }
+      }
+      return count;
+    }
   }]
 );
